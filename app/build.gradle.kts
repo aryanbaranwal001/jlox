1db5dde8824f55f147a9ed63b81fd51b643b8e64
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-06-11 13:55:09 +0530

    fix: stdin forwarding for ./gradlew run

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,10 @@ application {
     mainClass = "jlox.Lox"
 }
 
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
+
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
