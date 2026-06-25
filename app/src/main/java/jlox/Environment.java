bc20ea5d260177d8a57a97fb7cb6287a2ac32c51
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-06-25 14:08:03 +0530

    refactor: restructure Environment.java

--- app/src/main/java/jlox/Environment.java
+++ app/src/main/java/jlox/Environment.java
@@ -6,6 +6,10 @@ import java.util.Map;
 class Environment {
   private final Map<String, Object> values = new HashMap<>();
 
+  void define(String name, Object value) {
+    values.put(name, value);
+  }
+
   Object get(Token name) {
     if (values.containsKey(name.lexeme)) {
       return values.get(name.lexeme);
@@ -19,9 +23,5 @@ class Environment {
       return;
     }
     throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
-  }
-
-  void define(String name, Object value) {
-    values.put(name, value);
   }
 }
