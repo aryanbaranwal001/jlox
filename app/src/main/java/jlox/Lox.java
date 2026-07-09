b278257dc896f381503662e03cb4370671acc057
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-07-09 19:55:30 +0530

    feat: disallow top level return statements

--- app/src/main/java/jlox/Lox.java
+++ app/src/main/java/jlox/Lox.java
@@ -81,6 +81,8 @@ public class Lox {
     Resolver resolver = new Resolver(interpreter);
     resolver.resolve(statements);
 
+    if (hadError) return;
+
     interpreter.interpret(statements);
   }
 
