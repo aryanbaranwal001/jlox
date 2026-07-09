8b160c98e9a1dbfc9818b3ee1e646c7946cd2929
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-07-09 16:06:37 +0530

    feat: add semantic resolver to bind variables before interpreting

--- app/src/main/java/jlox/Interpreter.java
+++ app/src/main/java/jlox/Interpreter.java
@@ -1,11 +1,14 @@
 package jlox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 // C We use Void instead of void because void can't be used as a generic type argument.
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   final Environment globals = new Environment();
+  private final Map<Expr, Integer> locals = new HashMap<>();
   private Environment environment = globals;
 
   Interpreter() {
@@ -151,7 +154,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   @Override
   public Object visitAssignExpr(Expr.Assign expr) {
     Object value = evaluate(expr.value);
-    environment.assign(expr.name, value);
+
+    Integer distance = locals.get(expr);
+    if (distance != null) {
+      environment.assignAt(distance, expr.name, value);
+    } else {
+      globals.assign(expr.name, value);
+    }
+
     return value;
   }
 
@@ -232,10 +242,23 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   @Override
   public Object visitVariableExpr(Expr.Variable expr) {
-    return environment.get(expr.name);
+    return lookUpVariable(expr.name, expr);
   }
 
   // Helpers
+
+  private Object lookUpVariable(Token name, Expr expr) {
+    Integer distance = locals.get(expr);
+    if (distance != null) {
+      return environment.getAt(distance, name.lexeme);
+    } else {
+      return globals.get(name);
+    }
+  }
+
+  void resolve(Expr expr, int depth) {
+    locals.put(expr, depth);
+  }
 
   void executeBlock(List<Stmt> statements, Environment environment) {
     Environment previous = this.environment;
